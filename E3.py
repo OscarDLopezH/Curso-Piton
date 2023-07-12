8b091ab4f641eff512c1dea85915f9a9b9636786
Oscar Lopez <a311022@alumnos.uaslp.mx>
2023-07-11 22:31:09 -0600

    E3

---
+++ E3.py
@@ -0,0 +1,9 @@
+##E3
+# Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética
+# 3 más 2, divido por la multiplicación de 2 por 5, el resultado elevado al cuadrado
+Mensaje1 = "Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética\n" 
+Mensaje2 =  "3 más 2, divido por la multiplicación de 2 por 5, el resultado elevado al cuadrado"
+Resultado = (3+2)/(2*5)
+Resultado = pow(Resultado,2)
+print(Mensaje1+" El resultado de "+ Mensaje2+ " es:")
+print(Resultado)
